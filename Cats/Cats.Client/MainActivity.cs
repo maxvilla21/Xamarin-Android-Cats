74b1d788cc3beacbc7c9b4aab0e5dc188d5fafd7
Maximiliano Villa <max_210291@hotmail.com>
2017-07-07 19:47:45 -0500

    Code1

---
+++ Cats/Cats.Client/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Cats.Client
+{
+    [Activity(Label = "Cats.Client", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
